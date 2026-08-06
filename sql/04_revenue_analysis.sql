29be8313677201c6da0c1ed4b7d9604d44ecff43
= <=>
2026-08-06 16:39:50 +0530

    feat: add revenue analysis with executive KPIs and business insights

---
+++ sql/04_revenue_analysis.sql
@@ -0,0 +1,133 @@
+-- 1. Total Revenue
+-- How much total revenue has the platform generated from all course enrollments?
+
+SELECT 
+	SUM(c.price) Total_Revenue
+FROM
+	dbo.courses c 
+JOIN 
+	dbo.enrollments e 
+ON 
+	c.course_id = e.course_id ;
+
+-- 2. Total Paid Enrollments
+
+SELECT
+	COUNT(*) Total_Enrollments
+FROM
+	dbo.enrollments e ;
+
+-- 3. Average Revenue per Enrollment
+
+SELECT 
+	CAST(AVG(c.price) AS Decimal(10,2)) avg_revenue_per_enrollment
+FROM
+	dbo.enrollments e 
+JOIN 
+	dbo.courses c 
+ON 
+	e.course_id = c.course_id;
+
+-- 4. Monthly Revenue Trend
+
+SELECT 
+	YEAR(e.enrollment_date) revenue_year,
+	MONTH(e.enrollment_date) revenue_month,
+	DATENAME(MONTH, e.enrollment_date) month_name,
+	SUM(c.price) total_revenue
+FROM
+	dbo.enrollments e 
+JOIN
+	dbo.courses c 
+ON
+	e.course_id = c.course_id 
+GROUP BY 
+	YEAR(e.enrollment_date),
+	MONTH(e.enrollment_date),
+	DATENAME(MONTH, e.enrollment_date)
+ORDER BY
+	revenue_year,
+	revenue_month;
+
+-- 5.Revenue by Category
+
+SELECT
+	c.category,
+	SUM(c.price) total_revenue,
+	COUNT(e.enrollment_id) total_enrollments
+FROM
+	dbo.courses c 
+JOIN 
+	dbo.enrollments e 
+ON
+	c.course_id = e.course_id 
+GROUP BY 
+	c.category
+ORDER BY 
+	total_revenue DESC ;
+
+-- 6.Revenue by Instructor
+
+SELECT
+	i.instructor_name Instructor,
+	SUM(c.price) total_revenue,
+	COUNT(e.enrollment_id) total_enrollments
+FROM
+	dbo.instructors i 
+JOIN 
+	dbo.courses c
+ON 
+	i.instructor_id = c.instructor_id
+JOIN 
+	dbo.enrollments e 
+ON 
+	c.course_id = e.course_id 
+GROUP BY
+	i.instructor_name 
+ORDER BY 
+	total_revenue DESC;
+
+-- 7.Top 10 Revenue generating Courses
+
+SELECT
+	TOP 10
+	c.course_name Course,
+	c.category Category,
+	SUM(c.price) total_revenue,
+	COUNT(e.enrollment_id) total_enrollments
+FROM 
+	dbo.courses c 
+JOIN 	
+	dbo.enrollments e 
+ON
+	c.course_id = e.course_id 
+GROUP BY 
+	c.course_name,
+	c.category 
+ORDER BY
+	total_revenue DESC;
+
+-- 8.Revenue Contribution by Category
+
+SELECT
+
+    c.category,
+
+    SUM(c.price) AS total_revenue,
+
+    ROUND(
+        100.0 * SUM(c.price)
+        / SUM(SUM(c.price)) OVER (),
+        2
+    ) AS revenue_percentage
+
+FROM courses c
+
+JOIN enrollments e
+ON c.course_id=e.course_id
+
+GROUP BY
+    c.category
+
+ORDER BY
+    total_revenue DESC;
