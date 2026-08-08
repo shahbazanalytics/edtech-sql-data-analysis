0f9c1bdeef264f21a67253c4503ed4896d184a95
= <=>
2026-08-08 07:08:47 +0530

    feat(sql): add instructor analysis with revenue, reach, and performance insights

---
+++ sql/07_instructor_analysis.sql
@@ -0,0 +1,92 @@
+-- 1.Which instructors generate the highest revenue?
+
+SELECT
+	i.instructor_name,
+	COUNT(e.enrollment_id) total_enrollments,
+	SUM(c.price) total_revenue
+FROM
+	dbo.instructors i 
+JOIN 
+	dbo.courses c 
+ON 
+	i.instructor_id = c.instructor_id 
+JOIN
+	dbo.enrollments e 
+ON
+	c.course_id = e.course_id 
+GROUP BY
+	i.instructor_name
+ORDER BY
+	total_revenue DESC;
+
+-- 2.Students Per Instructor
+
+SELECT
+    i.instructor_id,
+    i.instructor_name,
+    COUNT(DISTINCT e.student_id) AS total_students
+FROM instructors i
+JOIN courses c
+    ON i.instructor_id = c.instructor_id
+JOIN enrollments e
+    ON c.course_id = e.course_id
+GROUP BY
+    i.instructor_id,
+    i.instructor_name
+ORDER BY
+    total_students DESC;
+
+-- 3. Instructor Performance Summary
+
+WITH InstructorCourseMetrics AS
+(
+    SELECT
+        c.course_id,
+        c.instructor_id,
+        c.price,
+        COUNT(e.enrollment_id) AS total_enrollments,
+        COUNT(DISTINCT e.student_id) AS total_students,
+        SUM(c.price) AS course_revenue
+    FROM courses c
+    LEFT JOIN enrollments e
+        ON c.course_id = e.course_id
+    GROUP BY
+        c.course_id,
+        c.instructor_id,
+        c.price
+)
+
+SELECT
+    i.instructor_id,
+    i.instructor_name,
+
+    COUNT(icm.course_id) AS total_courses,
+
+    SUM(icm.total_enrollments) AS total_enrollments,
+
+    SUM(icm.total_students) AS total_students,
+
+    SUM(icm.course_revenue) AS total_revenue,
+
+    ROUND(
+        AVG(icm.total_enrollments * 1.0),
+        2
+    ) AS avg_enrollments_per_course,
+
+    ROUND(
+        SUM(icm.course_revenue * 1.0)
+        / NULLIF(COUNT(icm.course_id), 0),
+        2
+    ) AS avg_revenue_per_course
+
+FROM instructors i
+
+LEFT JOIN InstructorCourseMetrics icm
+    ON i.instructor_id = icm.instructor_id
+
+GROUP BY
+    i.instructor_id,
+    i.instructor_name
+
+ORDER BY
+    total_revenue DESC;
