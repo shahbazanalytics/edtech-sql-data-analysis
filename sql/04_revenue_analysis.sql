93a0cb58ba9c5ab8114327532bf450e3eefa975e
= <=>
2026-08-08 20:28:55 +0530

    Update Revenue Analysis

--- sql/04_revenue_analysis.sql
+++ sql/04_revenue_analysis.sql
@@ -30,41 +30,78 @@ ON
 
 -- 4. Monthly Revenue Trend
 
-SELECT 
-	YEAR(e.enrollment_date) revenue_year,
-	MONTH(e.enrollment_date) revenue_month,
-	DATENAME(MONTH, e.enrollment_date) month_name,
-	SUM(c.price) total_revenue
-FROM
-	dbo.enrollments e 
-JOIN
-	dbo.courses c 
-ON
-	e.course_id = c.course_id 
-GROUP BY 
-	YEAR(e.enrollment_date),
-	MONTH(e.enrollment_date),
-	DATENAME(MONTH, e.enrollment_date)
+WITH CourseMonthlyRevenue AS
+(
+    SELECT
+        YEAR(e.enrollment_date) AS revenue_year,
+        MONTH(e.enrollment_date) AS revenue_month,
+        DATENAME(MONTH, e.enrollment_date) AS month_name,
+        e.course_id,
+        c.price,
+        COUNT(e.enrollment_id) AS total_enrollments
+    FROM 
+    	enrollments e
+JOIN 
+	courses c
+ON 
+	e.course_id = c.course_id
+GROUP BY
+    YEAR(e.enrollment_date),
+    MONTH(e.enrollment_date),
+    DATENAME(MONTH, e.enrollment_date),
+    e.course_id,
+    c.price
+)
+
+SELECT
+    revenue_year,
+    revenue_month,
+    month_name,
+    SUM(price * total_enrollments) AS total_revenue
+FROM 
+	CourseMonthlyRevenue
+GROUP BY
+    revenue_year,
+    revenue_month,
+    month_name
 ORDER BY
-	revenue_year,
-	revenue_month;
+    revenue_year,
+    revenue_month;
 
 -- 5.Revenue by Category
 
+WITH CourseCategoryRevenue AS
+(
+    SELECT
+        c.course_id,
+        c.category,
+        c.price,
+        COUNT(e.enrollment_id) AS total_enrollments
+    FROM 
+    	courses c
+    JOIN enrollments e
+    ON 
+    	c.course_id = e.course_id
+    GROUP BY
+        c.course_id,
+        c.category,
+        c.price
+)
+
 SELECT
-	c.category,
-	SUM(c.price) total_revenue,
-	COUNT(e.enrollment_id) total_enrollments
-FROM
-	dbo.courses c 
-JOIN 
-	dbo.enrollments e 
-ON
-	c.course_id = e.course_id 
-GROUP BY 
-	c.category
-ORDER BY 
-	total_revenue DESC ;
+    category,
+
+    SUM(total_enrollments) AS total_enrollments,
+
+    SUM(price * total_enrollments) AS total_revenue
+
+FROM CourseCategoryRevenue
+
+GROUP BY
+    category
+
+ORDER BY
+    total_revenue DESC;
 
 -- 6.Revenue by Instructor
 
